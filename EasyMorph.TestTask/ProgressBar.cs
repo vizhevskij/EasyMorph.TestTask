5407913961093f4dfc57cc542f6262e813bf56d4
Vitaly <moyavital@gmail.com>
2026-07-30 23:49:12 +0300

    Initial commit

---
+++ EasyMorph.TestTask/ProgressBar.cs
@@ -0,0 +1,43 @@
+﻿namespace EasyMorph.TestTask
+{
+    public sealed class ProgressBar
+    {
+        private readonly int _total;
+        private readonly int _stars;
+
+        private int _current;
+        private int _printed;
+
+        public ProgressBar(int total, int stars = 50)
+        {
+            _total = total;
+            _stars = stars;
+
+            Console.Write('[');
+        }
+
+        public void Increment()
+        {
+            _current++;
+
+            var shouldPrint = _current * _stars / _total;
+
+            while (_printed < shouldPrint)
+            {
+                Console.Write('*');
+                _printed++;
+            }
+        }
+
+        public void Finish()
+        {
+            while (_printed < _stars)
+            {
+                Console.Write('*');
+                _printed++;
+            }
+
+            Console.WriteLine("] Done");
+        }
+    }
+}
---
+++ EasyMorph.UnitTests/UnitTests.cs
@@ -0,0 +1,37 @@
+﻿using EasyMorph.TestTask;
+using System.Xml;
+
+namespace EasyMorph.UnitTests
+{
+    public class UnitTests
+    {
+        [Theory]
+        [InlineData("TotalAmount\\First", "2026_07_26_out.xml", "/Period[@Date='2026-07-26']/Store[@Name='ABC']/TotalAmount", "666.66")]
+        [InlineData("TotalAmount\\UnclosedTag", null, null, null, "The 'Product' start tag on line 4 position 4 does not match the end tag")]
+        [InlineData("TotalAmount\\SeveralFiles", "2026_07_26_out.xml", "/Period[@Date='2026-07-26']/Store[@Name='ABC']/TotalAmount", "1718.19")]
+        [InlineData("TotalAmount\\InvalidTotalAmounts", "2026_07_26_out.xml", "/Period[@Date='2026-07-26']/Store[@Name='ABC']/TotalAmount", "543.21", "123.457")]
+        [InlineData("TotalAmount\\InvalidTotalAmounts", "2026_07_26_out.xml", "/Period[@Date='2026-07-26']/Store[@Name='ABC']/TotalAmount", "543.21", "123,45")]
+        [InlineData("TotalAmount\\InvalidTotalAmounts", "2026_07_26_out.xml", "/Period[@Date='2026-07-26']/Store[@Name='ABC']/TotalAmount", "543.21", "-123.45")]
+        [InlineData("TotalAmount\\InvalidTotalAmounts", "2026_07_26_out.xml", "/Period[@Date='2026-07-26']/Store[@Name='ABC']/TotalAmount", "543.21", "aaaa.bbb")]
+        [InlineData("TotalAmount\\InvalidPeriods", null, null, null, "2026-26-07")]
+
+        public void CheckTotalAmount(string workDir, string resFile, string xPath, string expected, string expectedError = null)
+        {
+            var startDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, workDir);
+            var parser = new Parser(startDir);
+            parser.Run();
+            if (resFile != null && xPath != null && expected != null)
+            {
+                var resDoc = new XmlDocument();
+                resDoc.Load(Path.Combine(startDir, resFile));
+                var expectedNode = resDoc.DocumentElement.SelectSingleNode(xPath);
+                Assert.Equal(expected, expectedNode.InnerText);
+            }
+            if (!string.IsNullOrEmpty(expectedError))
+            {
+                var errors = File.ReadAllText(Path.Combine(startDir, "errors.txt"));
+                Assert.Contains(expectedError, errors);
+            }
+        }
+    }
+}
